b7bba3ee3cc3e01effa85bf2cd6259eec9512e28
Andrey Donets <donets.andre@gmail.com>
2024-01-18 18:17:24 +0400

    feat: pass global colors to firefox

--- modules/home/applications/firefox/default.nix
+++ modules/home/applications/firefox/default.nix
@@ -279,7 +279,33 @@ in {
       # Add custom modifications
       "${chrome}/customContent.css".source = ./custom/customContent.css;
       "${chrome}/customChrome.css".source = ./custom/customChrome.css;
-      "${chrome}/custom/pages".source = ./custom/pages;
+      "${chrome}/custom/pages" = {
+        source = ./custom/pages;
+        recursive = true;
+      }
+      "${chrome}/custom/pages/customColors.css".text = ''
+        :root {
+          --nova-background-dark: ${palette.background.dark} !important;
+          --nova-background-dim: ${palette.background.dim} !important;
+          --nova-background-normal: ${palette.background.normal} !important;
+          --nova-background-shiny: ${palette.background.shiny} !important;
+          --nova-background-bright: ${palette.background.bright} !important;
+          --nova-background-vivid: ${palette.background.vivid} !important;
+
+          --nova-foreground-dim: ${palette.foreground.dim} !important;
+          --nova-foreground-normal: ${palette.foreground.normal} !important;
+          --nova-foreground-bright: ${palette.foreground.bright} !important;
+
+          --nova-accent-red: ${palette.accents.red} !important;
+          --nova-accent-green: ${palette.accents.green} !important;
+          --nova-accent-yellow: ${palette.accents.yellow} !important;
+          --nova-accent-blue: ${palette.accents.blue} !important;
+          --nova-accent-magenta: ${palette.accents.magenta} !important;
+          --nova-accent-cyan: ${palette.accents.cyan} !important;
+
+        }
+      '';
+
       #"${chrome}/custom/interface".source = ./custom/interface;
     };
 
